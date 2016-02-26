1a46ad9501a39e47549db227b51e29debe67dc59
lelea2 <daomaikhanh1988@yahoo.com>
2016-02-25 21:33:05 -0800

    Initial QR code checkin

--- server/base/index.js
+++ server/base/index.js
@@ -1,5 +1,11 @@
 // Base routes for default index/root path, about page, 404 error pages, and others..
+var qr = require('qr-image');
+
+//var qr_svg = qr.image('I love QR!', { type: 'svg' });
+//qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+
 exports.register = function(server, options, next) {
+
     server.route([
         {
             method: 'GET',
@@ -7,8 +13,14 @@ exports.register = function(server, options, next) {
             config: {
                 handler: function(request, reply){
                   // Render the view with the custom greeting
+                    //var test = qr.imageSync('I love QR!', { type: 'svg' });
+                    //console.log(test);
+                    //var svg_string = qr.svgObject('I love QR!', { type: 'svg' });
+                    var svg_obj = qr.svgObject('San Jose Center For The Performing Arts', { type: 'svg' });
+                    //console.log(svg_string);
                     reply.view('index', {
-                        title: 'Awesome Boilerplate Homepage'
+                        title: 'Welcome to smart tree homepage',
+                        qrcode: svg_obj.path
                     });
                 },
                 id: 'index'
