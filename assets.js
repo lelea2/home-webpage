be0cc3977035876839069cc6f8855e23e4cfb667
Khanh Dao <kdao@Khanhs-MBP.attlocal.net>
2016-05-03 21:56:15 -0700

    Initial check in to fix css on prod

--- assets.js
+++ assets.js
@@ -5,7 +5,9 @@ module.exports = {
         css: ['css/test-one.css', 'css/test-two.css']
     },
     production: {
-        js: ['js/scripts.js'],
-        css: ['css/styles.css']
+        js: ['js/test-one.js', 'js/test-two.js'],
+        css: ['css/test-one.css', 'css/test-two.css']
+        //js: ['js/scripts.js'],
+        //css: ['css/styles.css']
     }
 }
